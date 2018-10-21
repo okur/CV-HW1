b4d0a412bface730c55584fedf853ba36504fe82
okur <okuabd@gmail.com>
2018-10-21 15:29:25 +0300

    opening image is added.

--- GUI.py
+++ GUI.py
@@ -6,8 +6,10 @@ Created on Sun Oct 14 17:52:37 2018
 @author: okur
 """
 
+import histogram
 import sys
-from PyQt5.QtWidgets import QLabel, QApplication, QWidget, QMainWindow, QAction, qApp, QVBoxLayout, QFrame, QSplitter,QHBoxLayout
+from PyQt5.QtWidgets import ( QLabel, QApplication, QWidget, QMainWindow, QAction, qApp,
+    QVBoxLayout, QFrame, QFileDialog, QHBoxLayout, QGroupBox, QGridLayout, QDesktopWidget)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 
@@ -18,9 +20,8 @@ class appWindow(QMainWindow):
         self.showWindow()
     
     def showWindow(self):
-        self.setWindowTitle('Histogram Matching')    
+        self.setWindowTitle('Histogram Matching')
         self.showMaximized()
-        
         
     def startMenu(self):
         quitBar = QAction('Exit', self) 
@@ -30,10 +31,12 @@ class appWindow(QMainWindow):
         
         inputBar = QAction('Open Input', self)
         inputBar.setStatusTip('Open Input Image')
+        self.inputImage = inputBar.triggered.connect(self.openInputImage)
         
         targetBar = QAction('Open Target', self)
         targetBar.setStatusTip('Open Target Image')
-
+        self.targetImage = targetBar.triggered.connect(self.openTargetImage)
+        
         equalizeButton = QAction('Equalize Histogram', self)
         equalizeButton.triggered.connect(self.equalizeHistogram)
         equalizeButton.setStatusTip('Equalize Histogram')
@@ -50,40 +53,68 @@ class appWindow(QMainWindow):
         widget = QWidget()
         self.setWidget(widget)
         self.setCentralWidget(widget)
+    
+    def openInputImage(self):
+        imagePath, _ = QFileDialog.getOpenFileName()
+        histogram.read_image(imagePath)
+    
+    def openTargetImage(self):
+        imagePath, _ = QFileDialog.getOpenFileName()
+        image = histogram.read_image(imagePath)
         
     def equalizeHistogram(self):
         print('abdullah')
     
     
     def setWidget(self, widget):     
-        widgetLayout = QVBoxLayout(widget)
+        widgetLayout = QGridLayout(widget)
         
-        frame1 = QFrame(widget)
-        frame1.setFrameShape(QFrame.StyledPanel)
+        box1 = QGroupBox(widget)
+        box1.setTitle('Input')
         
-        label1 = QLabel(widget)
-        label1.setText('Input')
+        vbox1 = QVBoxLayout(box1)
+        label11 = QLabel()
+        label11.setPixmap(QPixmap(self.inputImage))
+        label12 = QLabel()
+        label12.setPixmap(QPixmap("redrock.png"))
+        vbox1.addWidget(label11)
+        vbox1.addWidget(label12)
+        vbox1.addStretch()
+        box1.setLayout(vbox1)
         
-        frame2 = QFrame(widget)
-        frame2.setFrameShape(QFrame.StyledPanel)
-
-        label2 = QLabel(widget)
-        label2.setText('Target')
+        box2 = QGroupBox(widget)
+        box2.setTitle('Output')
         
-        frame3 = QFrame(widget)
-        frame3.setFrameShape(QFrame.StyledPanel)
+        vbox2 = QVBoxLayout(box2)
+        label21 = QLabel()
+        label21.setPixmap(QPixmap(self.targetImage))
+        label22 = QLabel()
+        label22.setPixmap(QPixmap("redrock.png"))
+        vbox2.addWidget(label21)
+        vbox2.addWidget(label22)
+        vbox2.addStretch()
+        box2.setLayout(vbox2)
         
-        label3 = QLabel(widget)
-        label3.setText('Result')
         
-        widgetLayout.addWidget(label1)
-        widgetLayout.addWidget(label2)
-        widgetLayout.addWidget(label3)
-        widgetLayout.addWidget(frame1)
-        widgetLayout.addWidget(frame2)
-        widgetLayout.addWidget(frame3)
+        box3 = QGroupBox(widget)
+        box3.setTitle('Result')
+        
+        vbox3 = QVBoxLayout(box3)
+        label31 = QLabel()
+        label31.setPixmap(QPixmap("redrock.png"))
+        label32 = QLabel()
+        label32.setPixmap(QPixmap("redrock.png"))
+        vbox3.addWidget(label31)
+        vbox3.addWidget(label32)
+        vbox3.addStretch()
+        box3.setLayout(vbox3)
+        
+        widgetLayout.addWidget(box1,0,0)
+        widgetLayout.addWidget(box2,0,1)
+        widgetLayout.addWidget(box3,0,2)
         widget.setLayout(widgetLayout)
-
+        widget.show()
+        
 app = QApplication(sys.argv)
 application = appWindow()
 sys.exit(app.exec_())
